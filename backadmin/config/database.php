b336ce6b7bb54adeac5fd5b3eee8c013933178a7
tanxinchao <txc913@163.com>
2018-05-29 14:01:28 +0800

    提交vue run build

--- backadmin/config/database.php
+++ backadmin/config/database.php
@@ -3,13 +3,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '101.132.104.170',
+    'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'admin_v3',
+    'database'        => 'vuehsop',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => 'Bblf2017test',
+    'password'        => 'xinchao913',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
